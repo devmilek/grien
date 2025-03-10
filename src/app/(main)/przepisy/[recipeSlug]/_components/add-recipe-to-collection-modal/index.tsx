762cab499570b97e208538b741aa7e0b21bad5ac
devmilek <devmilek@gmail.com>
2025-03-10 19:25:34 +0100

    feat: add schemas and components for recipe comments, collections, and images; implement recipe actions and steps list

---
+++ src/app/(main)/przepisy/[recipeSlug]/_components/add-recipe-to-collection-modal/index.tsx
@@ -0,0 +1,140 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Bookmark } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import CreateCollectionPopover from "./create-collection-popover";
+import { useQuery } from "@tanstack/react-query";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { toast } from "sonner";
+import {
+  addRecipeToCollections,
+  toggleFavouriteRecipe,
+} from "@/actions/collections-recipes";
+import CollectionTile from "./collection-tile";
+import FavourtieCollectionTile from "./favourite-collection-tile";
+import { Separator } from "@/components/ui/separator";
+import { getUserCollections } from "./actions";
+
+const AddRecipeToCollectionModal = ({ recipeId }: { recipeId: string }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedCollections, setSelectedCollections] = useState<string[]>([]);
+  const [isFavourite, setIsFavourite] = useState(false);
+
+  const { data, refetch } = useQuery({
+    queryKey: ["collections"],
+    queryFn: async () => await getUserCollections({ recipeId }),
+    enabled: isOpen,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setSelectedCollections(data.recipesSavedInCollections);
+      setIsFavourite(data.isFavourite);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (isOpen) {
+      refetch();
+    }
+  }, [isOpen, refetch]);
+
+  const handleFavourite = async () => {
+    const { message, status } = await toggleFavouriteRecipe(
+      recipeId,
+      isFavourite
+    );
+
+    if (status !== 200) {
+      toast.error(message);
+    }
+  };
+
+  const handleSave = async () => {
+    await handleFavourite();
+    const { message, status } = await addRecipeToCollections(
+      recipeId,
+      selectedCollections
+    );
+
+    if (status === 200) {
+      toast.success(message);
+      setIsOpen(false);
+    } else {
+      toast.error(message);
+    }
+  };
+
+  return (
+    <Dialog
+      open={isOpen}
+      onOpenChange={(val) => {
+        setIsOpen(val);
+      }}
+    >
+      <DialogTrigger asChild>
+        <Button variant="ghost">
+          <Bookmark />
+          <span className="lg:hidden xl:inline-block">Zapisz</span>
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Zapisz przepis</DialogTitle>
+          <DialogDescription>
+            Wybierz kolekcję, do której chcesz zapisać przepis lub zapisz w
+            polubionych
+          </DialogDescription>
+        </DialogHeader>
+        <ScrollArea className="max-h-80">
+          {data && (
+            <div>
+              <FavourtieCollectionTile
+                count={data.favouritesCount}
+                checked={isFavourite}
+                onSelect={() => setIsFavourite(!isFavourite)}
+              />
+              <Separator className="my-3" />
+              {data.collections.map((collection) => (
+                <CollectionTile
+                  key={collection.id}
+                  name={collection.name}
+                  count={collection.recipesCount}
+                  checked={selectedCollections.includes(collection.id)}
+                  onSelect={() => {
+                    if (selectedCollections.includes(collection.id)) {
+                      setSelectedCollections(
+                        selectedCollections.filter((id) => id !== collection.id)
+                      );
+                    } else {
+                      setSelectedCollections([
+                        ...selectedCollections,
+                        collection.id,
+                      ]);
+                    }
+                  }}
+                />
+              ))}
+            </div>
+          )}
+        </ScrollArea>
+        <DialogFooter>
+          <CreateCollectionPopover />
+          <Button onClick={handleSave}>Gotowe</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default AddRecipeToCollectionModal;
---
+++ src/app/(main)/przepisy/[recipeSlug]/page.tsx
@@ -0,0 +1,147 @@
+import db from "@/db";
+import { recipeLikes, recipes } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { notFound } from "next/navigation";
+import React from "react";
+import IngredientsList from "./_components/ingredients-list";
+import StepsList from "./_components/steps-list";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import CommentsCard from "./_components/comments-card";
+import { getCurrentSession } from "@/lib/auth/utils";
+import RecipeImage from "./_components/recipe-image";
+import RecipeActions from "./_components/recipe-actions";
+import RecipeAttributes from "./_components/recipe-attributes";
+import RecipeHeader from "./_components/recipe-header";
+import RecipeLicence from "./_components/recipe-licence";
+import { constructMetadata } from "@/utils/construct-metadata";
+import { Metadata } from "next";
+
+interface RecipePageProps {
+  params: Promise<{ recipeSlug: string }>;
+}
+
+export async function generateMetadata({
+  params,
+}: RecipePageProps): Promise<Metadata> {
+  const slug = (await params).recipeSlug;
+  const recipe = await db.query.recipes.findFirst({
+    where: eq(recipes.slug, slug),
+    with: {
+      user: true,
+      category: true,
+      image: true,
+    },
+  });
+
+  if (!recipe) {
+    return {};
+  }
+
+  return constructMetadata({
+    title: recipe.name,
+    description: recipe.description,
+    image: recipe.image?.url,
+    url: `/${recipe.category.slug}/${recipe.slug}`,
+    authors: {
+      name: recipe.user.name,
+      url: `/kucharze/${recipe.user.username}`,
+    },
+    category: recipe.category.name,
+  });
+}
+
+const RecipePage = async ({ params }: RecipePageProps) => {
+  const { user } = await getCurrentSession();
+  const slug = (await params).recipeSlug;
+  const recipe = await db.query.recipes.findFirst({
+    where: eq(recipes.slug, slug),
+    with: {
+      image: {
+        columns: {
+          url: true,
+        },
+      },
+      user: true,
+      ingredients: true,
+      category: true,
+      steps: {
+        with: {
+          image: true,
+        },
+      },
+      licence: true,
+      attributes: {
+        with: {
+          attribute: true,
+        },
+      },
+    },
+  });
+
+  if (!recipe) {
+    notFound();
+  }
+
+  const likes = await db.$count(
+    recipeLikes,
+    eq(recipeLikes.recipeId, recipe.id)
+  );
+
+  return (
+    <div className="mx-auto container">
+      {/* hero */}
+      <TooltipProvider>
+        <section className="grid gap-4 lg:grid-cols-2 lg:gap-8 md:min-h-96 bg-white rounded-xl p-4 md:p-6">
+          {recipe.image && (
+            <RecipeImage imageUrl={recipe.image.url} licence={recipe.licence} />
+          )}
+          <div className="flex flex-col">
+            <div className="flex-1">
+              <RecipeHeader
+                name={recipe.name}
+                createdAt={recipe.createdAt}
+                author={{
+                  name: recipe.user.name,
+                }}
+              />
+              {/* ATTRIBUTES */}
+              <RecipeAttributes
+                attributes={recipe.attributes}
+                category={recipe.category}
+                difficulty={recipe.difficulty}
+                portions={recipe.portions}
+                preparationTime={recipe.preparationTime}
+              />
+              {/* DESC */}
+              <p className="text-muted-foreground text-sm mt-4">
+                {recipe.description}
+              </p>
+            </div>
+            <RecipeActions
+              recipe={recipe}
+              steps={recipe.steps}
+              likes={likes}
+              userId={user?.id}
+            />
+          </div>
+        </section>
+      </TooltipProvider>
+      {/* LAYOUT */}
+      <div className="flex gap-6 mt-6 flex-col lg:flex-row">
+        <div className="lg:w-[380px]">
+          <IngredientsList
+            ingredients={recipe.ingredients}
+            portions={recipe.portions}
+          />
+        </div>
+        <div className="flex-1 space-y-8">
+          <StepsList steps={recipe.steps} />
+          {recipe.licence && <RecipeLicence licence={recipe.licence} />}
+          <CommentsCard recipeId={recipe.id} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RecipePage;
